b64fc8493666d613c52e95ac54a978f4b9990d1c
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-04-17 09:49:24 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,9 @@ import Tab from "./Tab";
 
 const App = () => {
   const [items] = useState([
-    { title: "Tab 1", content: "This is the content for Tab 1" },
-    { title: "Tab 2", content: "This is the content for Tab 2" },
-    { title: "Tab 3", content: "This is the content for Tab 3" },
+    { title: "Tab 1", content: "This is the content for Tab 1." },
+    { title: "Tab 2", content: "This is the content for Tab 2." },
+    { title: "Tab 3", content: "This is the content for Tab 3." },
   ]);
   const [activeTab,setActiveTab] = useState(0);
 
